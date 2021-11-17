cc2ff54db185f277cf54cc7e87d53c891a7ae0c4
jjachowski <84469968+jjachowski@users.noreply.github.com>
2021-11-17 22:28:42 +0100

    changed introduction (#1)
    Co-authored-by: Jakub Jachowski <>

--- pages/index.jsx
+++ pages/index.jsx
@@ -19,7 +19,7 @@ const Home = ({ allPostsData }) => (
       <title>{siteTitle}</title>
     </Head>
     <section className={utilStyles.headingMd}>
-      <p>me is pepe</p>
+      <p>Hi! My name is</p>
       <p>
         (This is a sample website - you’ll be building a site like this on{' '}
         <a href='https://nextjs.org/learn'>our Next.js tutorial</a>.)
